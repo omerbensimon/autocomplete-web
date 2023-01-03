32e43e511b794bec79b1797a496fcd56f76c9912
omerbensimon <omerbensimon3005@gmail.com>
2023-01-03 21:16:38 +0200

    remove spaces

--- src/app/components/SearchBar/SearchBar.js
+++ src/app/components/SearchBar/SearchBar.js
@@ -15,16 +15,16 @@ function SearchBar({ clicked, setClicked }) {
 
     const changeHandler = async (e) => {
         setSearch(e.target.value)
-        if (e.target.value.length <= 1) {
+        if (e.target.value.trim().length <= 1) {
             setShowDropdown(false)
         }
         let employees = []
-        if (e.target.value.length >= 2 && employees.length === 0) {
+        if (e.target.value.trim().length >= 2 && employees.length === 0) {
             employees = await fetchUsers(e.target.value)
             setEmployees([...employees])
         }
         setFilteredEmployees([...employees].filter((emp) => {
-            return emp.name.toLowerCase().includes(e.target.value.toLowerCase()) || emp.workTitle.includes(e.target.value.toLowerCase())
+            return emp.name.toLowerCase().includes(e.target.value.trim().toLowerCase()) || emp.workTitle.includes(e.target.value.trim().toLowerCase())
         }))
     }
 
@@ -45,7 +45,7 @@ function SearchBar({ clicked, setClicked }) {
                         {
                             filteredEmployees.map((employee) => {
                                 return (
-                                    <Item key={employee._id} employee={employee} search={search} />
+                                    <Item key={employee._id} employee={employee} search={search.trim()} />
                                 )
                             })
                         }
@@ -57,7 +57,7 @@ function SearchBar({ clicked, setClicked }) {
                     {
                         filteredEmployees.map((employee) => {
                             return (
-                                <Item key={employee._id} employee={employee} search={search} />
+                                <Item key={employee._id} employee={employee} search={search.trim()} />
                             )
                         })
                     }
